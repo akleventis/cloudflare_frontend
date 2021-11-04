d2060fc6b9d93729663e88d8355897e069f75ff8
Alex Leventis <alex.leventis1@gmail.com>
2021-11-04 14:21:07 -0400

    add github links

--- src/header/Header.js
+++ src/header/Header.js
@@ -2,15 +2,16 @@ import React from "react";
 import "./header.css";
 
 class Header extends React.Component {
-
   render() {
     return (
-    <header>
+      <header>
         <div className="header-container">
-            <h2>Cloudflare Blog</h2>
+        <p className="backend"><a href='https://github.com/akleventis/cloudflare_frontend' target='_blank' rel='noreferrer'>Github Front-end (React)</a></p>
+          <h2>Cloudflare Blog</h2>
+          <p className="backend"> <a href='https://github.com/akleventis/cloudflare_backend' target='_blank' rel='noreferrer'>Github Back-end</a></p>
         </div>
-    </header>
-    )
+      </header>
+    );
   }
 }
 
